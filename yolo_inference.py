d199e0bac52c7fef8bc0220190a68f984ff7e57a
Owen Yang <owentheyanger@gmail.com>
2024-04-26 16:28:08 -0700

    trained keypoints model

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
-result = model.predict('input_videos/input_video.mp4', save= True)
+model = YOLO('yolov8x.pt')
+result = model.track('input_videos/input_video.mp4', conf = 0.2, save= True)
 print(result)
 print("boxes:")
 for box in result[0].boxes:
